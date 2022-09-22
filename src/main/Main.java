eee93216b586f4b6c4d446995bb85b6c8ae61c7e
Nicholas Balabis <balabisnicholas@gmail.com>
2022-09-22 11:39:36 -0600

    Set up basic Java FX 11 Project

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,13 +9,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(fxml file));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/FirstScreen.fxml"));
         stage.setTitle("First Screen");
         stage.setScene(new Scene(root, 800, 600));
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-        //test content
+
     }
 }
